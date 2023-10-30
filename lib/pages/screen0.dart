362ba77f9f01a241531ada2e6b0829aee1523a82
Magaly Canelo <magalycanelo@gmail.com>
2023-10-29 23:46:33 -0500

    Crear App

---
+++ lib/pages/screen0.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Screen0 extends StatelessWidget {
+  const Screen0({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('APP PRUEBA'),
+          backgroundColor: const Color(0xFFA51616),
+          // backgroundColor: Colors.green[500],
+        ),
+        body: const SingleChildScrollView(
+            child: Column(children: [
+          Text(
+            'Otomanopee One - Regular',
+            style: TextStyle(fontFamily: 'Otomanopee One'),
+          ),
+          Text(
+            'Montserrat - Regular',
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontWeight: FontWeight.w400),
+          ),
+          Text(
+            'Montserrat - Medium',
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontWeight: FontWeight.w500),
+          ),
+          Text(
+            'Montserrat - SemiBold',
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontWeight: FontWeight.w600),
+          )
+        ])));
+  }
+}
