4e55f7364eef24836c5c83ccd6250d2954eb3ac6
Magaly Canelo <magalycanelo@gmail.com>
2023-10-29 23:56:15 -0500

    Se añadio los screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/pages/screen0.dart';
+import 'package:app/pages/screen15.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Screen0(),
+      home: Screen15(),
     );
   }
 }
