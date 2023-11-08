fa24bb6cc7cfa389ecbc568aa3b2c230464e1afc
Jorge200326 <j20032612@gmail.com>
2023-11-08 10:52:25 -0500

    modificaciones en custom_bottom

--- lib/widgets/custom_bottoms.dart
+++ lib/widgets/custom_bottoms.dart
@@ -4,31 +4,46 @@ class CustomBottomS extends StatelessWidget {
   final String title;
   final double tam;
   final Widget destino;
+
   const CustomBottomS(
-      {super.key,
+    {
+      super.key,
       required this.title,
       required this.tam,
-      required this.destino});
+      required this.destino
+    }
+  );
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(const Color(0xFF9F51CA)),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 60.0),
-                )),
-            child: Text(
-              title,
-              style: TextStyle(fontSize: tam),
-            ),
-            onPressed: () => {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => destino))
-                }));
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF9F51CA)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 60.0
+            )
+          )
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: tam
+          ),
+        ),
+        onPressed: () => {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => destino
+            )
+          )
+        },
+      ),
+    );
   }
 }
