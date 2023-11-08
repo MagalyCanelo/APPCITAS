cd3a58c139981482cf5f9948aa7d3992dbbb2b4f
Jorge200326 <j20032612@gmail.com>
2023-11-08 10:57:32 -0500

    agregue  un nuevo widget para un container imagen

---
+++ lib/widgets/custom_container_imagen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class CustomContainerImage extends StatelessWidget {
+  final double tamanioContainer;
+  final String direccion;
+  final String titulo;
+  final double altoImagen;
+  final String descripcion;
+
+  const CustomContainerImage(
+    {
+      super.key,
+      required this.tamanioContainer,
+      required this.direccion,
+      required this.titulo,
+      required this.altoImagen,
+      required this.descripcion
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: tamanioContainer,
+      decoration: BoxDecoration(
+        color: const Color(0XFFFFF1F1),
+        borderRadius: BorderRadius.circular(5.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5.0,
+            spreadRadius: 1.0,
+            offset: Offset(0, 5)
+          )
+        ]
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(5.0)
+            ),
+            child: Image.asset(direccion,
+            height: altoImagen, 
+            width: double.infinity,
+            fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 29.0,
+            color: const Color(0XFFF23574),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  titulo,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontFamily: 'Montserrat'
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          Container(
+            alignment: Alignment.center,
+            width: 266.0,
+            child: Text(
+              descripcion,
+              style: const TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 12,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          ElevatedButton.icon(
+            onPressed: () {}, 
+            icon: const Icon(
+              Icons.calendar_month
+            ),
+            label: const Text(
+              'Agendar Cita'
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF9F51CA)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
