8351b051e42c2b6d467c9327e1fb8e75e78bb120
Jorge200326 <j20032612@gmail.com>
2023-11-08 10:54:34 -0500

    agregue un nuevo widget para lo botones de textos

---
+++ lib/widgets/custom_buttom_text.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomButtomText extends StatelessWidget {
+  final Widget destino;
+
+  const CustomButtomText(
+    {
+      super.key,
+      required this.destino
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => destino
+            )
+          );
+        },
+        style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all(
+            const Size(20.0, 5.0)
+          )
+        ),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.arrow_back_ios_new,
+              size: 18.0,
+              color: Color(0XFF000000),
+            ),
+            SizedBox(width: 10.0),
+            Text(
+              'Atras',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Color(0xFF7B7B7B)
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
