d24898f41a7ba8d9f6bb95ec6051487074352131
Jorge200326 <j20032612@gmail.com>
2023-11-08 10:59:55 -0500

    modificado en widget de imagen

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 class CustomImage extends StatelessWidget {
   final String path;
-  const CustomImage({super.key, required this.path});
+
+  const CustomImage(
+    {
+      super.key, 
+      required this.path
+    }
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(25.0),
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
         child: Image.asset(path),
