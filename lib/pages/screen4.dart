e84fcb9dbcb2ba00025e9d02da426ee0e67b49c8
Jorge200326 <j20032612@gmail.com>
2023-11-08 23:59:01 -0500

    ACTUALICE EL SCREEN04

--- lib/pages/screen4.dart
+++ lib/pages/screen4.dart
@@ -1,17 +1,12 @@
 import 'package:app/pages/drawer.dart';
-import 'package:app/pages/screen5.dart';
-import 'package:app/widgets/custom_input.dart';
-import 'package:app/widgets/custom_pass.dart';
+import 'package:app/widgets/custom_input_dos.dart';
+import 'package:app/widgets/custom_pass_dos.dart';
+import 'package:app/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
-class Screen4 extends StatefulWidget {
+class Screen4 extends StatelessWidget {
   const Screen4({super.key});
 
-  @override
-  State<Screen4> createState() => _Screen4State();
-}
-
-class _Screen4State extends State<Screen4> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,97 +14,91 @@ class _Screen4State extends State<Screen4> {
       appBar: AppBar(
         backgroundColor: const Color(0XFFF23574),
       ),
-      drawer: const Drawer(child: Midrawer()),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 28.0, right: 20.0, left: 20.0, bottom: 5.0),
-                child: Image.asset('assets/img/corazon.jpg',
-                    width: 355, height: 100),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child:
-                    Image.asset('assets/img/letra.jpg', width: 350, height: 70),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0, left: 20.0),
-                child: Row(
-                  children: [
-                    Container(
-                      color: const Color(0XFF9F51CA),
-                      width: 5.0,
-                      height: 33.0,
-                    ),
-                    const SizedBox(width: 10.0),
-                    const Text(
-                      "PERFIL",
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontFamily: 'Otomanopee One',
-                        color: Color(0XFF9F51CA),
-                      ),
-                    ),
-                  ],
+      drawer: const Drawer(child: Midrawer(),),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/img/corazon.jpg',
+              width: 110.0,
+              height: 110.0,
+            ),
+            Image.asset(
+              'assets/img/letra.jpg',
+              width: 200.0,
+            ),
+            const SizedBox(height: 5.0),
+            Row(
+              children: [
+                Container(
+                  color: const Color(0xFF6E2794),
+                  width: 5.0,
+                  height: 40.0,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => const Screen5()),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        "Editar",
-                        style: TextStyle(
-                          color: Color(0XFF9F51CA),
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15.0,
-                        ),
-                      ),
-                    ),
-                  ],
+                const SizedBox(width: 10.0),
+                const CustomText(
+                  title: 'PERFIL', 
+                  tam: 30, 
+                  color: 0XFF9F51CA, 
+                  fontWeight: FontWeight.normal, 
+                  fontFamily: 'Otomanopee One'
                 ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {}, 
+                  child: const Text(
+                    'Editar',
+                    style: TextStyle(
+                      color: Color(0XFF9F51CA)
+                    ),
+                  )
+                )
+              ],
+            ),
+            const CustomInputDos(
+              tipo: TextInputType.number, 
+              title: 'DNI', 
+              alto: 50.0
+            ),
+            const SizedBox(height: 15.0),
+            const CustomInputDos(
+              tipo: TextInputType.name, 
+              title: 'Nombres', 
+              alto: 50.0
+            ),
+            const SizedBox(height: 15.0),
+            const CustomInputDos(
+              tipo: TextInputType.name, 
+              title: 'Apellidos', 
+              alto: 50.0
+            ),
+            const SizedBox(height: 15.0),
+            const CustomInputDos(
+              tipo: TextInputType.number, 
+              title: 'Celular', 
+              alto: 50.0
+            ),
+            const SizedBox(height: 15.0),
+            const CustomInputDos(
+              tipo: TextInputType.emailAddress, 
+              title: 'Correo', 
+              alto: 50.0
+            ),
+            const SizedBox(height: 7.0),
+            const SizedBox(
+              height: 65.0,
+              child: CustomPassDos(
+                tipo: TextInputType.visiblePassword, 
+                hidden: true, 
+                title: 'Contraseña'
               ),
-              const CustomInput(
-                tipo: TextInputType.number,
-                title: 'DNI',
-              ),
-              const CustomInput(
-                tipo: TextInputType.text,
-                title: 'Nombres',
-              ),
-              const CustomInput(
-                tipo: TextInputType.text,
-                title: 'Apellidos',
-              ),
-              const CustomInput(
-                tipo: TextInputType.number,
-                title: 'Celular',
-              ),
-              const CustomInput(
-                tipo: TextInputType.emailAddress,
-                title: 'Correo',
-              ),
-              const CustomPass(
-                tipo: TextInputType.text,
-                hidden: true,
-                title: 'Contraseña',
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
