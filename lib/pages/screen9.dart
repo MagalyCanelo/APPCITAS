c580808dad084002623ee5da6a5bbc07f511e56d
Jorge200326 <j20032612@gmail.com>
2023-11-09 00:03:57 -0500

    AGREGUE EL SCREEN09

--- lib/pages/screen9.dart
+++ lib/pages/screen9.dart
@@ -1,14 +1,10 @@
-import 'package:app/pages/drawer.dart';
+import 'package:app/widgets/custom_container_desplegable.dart';
+import 'package:app/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
-class Screen9 extends StatefulWidget {
+class Screen9 extends StatelessWidget {
   const Screen9({super.key});
 
-  @override
-  State<Screen9> createState() => _Screen9State();
-}
-
-class _Screen9State extends State<Screen9> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,141 +12,39 @@ class _Screen9State extends State<Screen9> {
       appBar: AppBar(
         backgroundColor: const Color(0XFFF23574),
       ),
-      drawer: const Drawer(child: Midrawer()),
-      body: SingleChildScrollView(
-          child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    color: const Color(0xFF6E2794),
-                    width: 5.0,
-                    height: 40.0,
-                  ),
-                  const SizedBox(width: 10.0),
-                  const Text(
-                    "SERVICIOS",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'Otomanopee One',
-                        color: Color(0XFF9F51CA)),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFFFFF1F1)),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.asset(
-                              'assets/img/ecografiaObstetrica.jpg')),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "ECOGRAFÍA OBSTETRICA",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  minimumSize: const Size(20, 40),
-                                  backgroundColor: const Color(0XFF9F51CA)),
-                              onPressed: () {},
-                              child: const Icon(Icons.arrow_forward))
-                        ],
-                      )
-                    ],
-                  ),
+      drawer: Drawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  color: const Color(0xFF6E2794),
+                  width: 5.0,
+                  height: 40.0,
                 ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFFFFF1F1)),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.asset(
-                              'assets/img/ecografiaTransvaginal.jpg')),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "ECOGRAFÍA TRANSVAGINAL",
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  minimumSize: const Size(20, 40),
-                                  backgroundColor: const Color(0XFF9F51CA)),
-                              onPressed: () {},
-                              child: const Icon(Icons.arrow_forward))
-                        ],
-                      )
-                    ],
-                  ),
+                const SizedBox(width: 10.0),
+                const CustomText(
+                  title: 'SERVICIOS', 
+                  tam: 30, 
+                  color: 0XFF9F51CA, 
+                  fontWeight: FontWeight.normal, 
+                  fontFamily: 'Otomanopee One'
                 ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: const Color(0xFFFFF1F1)),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.asset('assets/img/controlPrenatal.jpg')),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "CONTROL PRENATAL",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  minimumSize: const Size(20, 40),
-                                  backgroundColor: const Color(0XFF9F51CA)),
-                              onPressed: () {},
-                              child: const Icon(Icons.arrow_forward))
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            const CustomContainerDesplegable(
+              path: 'assets/img/ecografiaObstetrica.jpg',
+              title: 'ECOGRAFÍA OBSTETRICA ',
+              descripcion1: '1. Historia Clínico Electrónica',
+              descripcion2: '2. Evaluación Materna y Fetal',
+              descripcion3: '3. Control Ecográfico Básico',
+            ),
+          ],
         ),
-      )),
+      )
     );
   }
 }
