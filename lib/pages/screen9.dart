cdeb8a8553e8e5406d91bc817c099159b293a8a6
Magaly Canelo <magalycanelo@gmail.com>
2023-11-11 21:10:32 -0500

    Se añadio Firebase

--- lib/pages/screen9.dart
+++ lib/pages/screen9.dart
@@ -8,43 +8,41 @@ class Screen9 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: const Color(0XFFF23574),
-      ),
-      drawer: Drawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  color: const Color(0xFF6E2794),
-                  width: 5.0,
-                  height: 40.0,
-                ),
-                const SizedBox(width: 10.0),
-                const CustomText(
-                  title: 'SERVICIOS', 
-                  tam: 30, 
-                  color: 0XFF9F51CA, 
-                  fontWeight: FontWeight.normal, 
-                  fontFamily: 'Otomanopee One'
-                ),
-              ],
-            ),
-            const SizedBox(height: 15.0),
-            const CustomContainerDesplegable(
-              path: 'assets/img/ecografiaObstetrica.jpg',
-              title: 'ECOGRAFÍA OBSTETRICA ',
-              descripcion1: '1. Historia Clínico Electrónica',
-              descripcion2: '2. Evaluación Materna y Fetal',
-              descripcion3: '3. Control Ecográfico Básico',
-            ),
-          ],
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0XFFF23574),
         ),
-      )
-    );
+        drawer: const Drawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    color: const Color(0xFF6E2794),
+                    width: 5.0,
+                    height: 40.0,
+                  ),
+                  const SizedBox(width: 10.0),
+                  const CustomText(
+                      title: 'SERVICIOS',
+                      tam: 30,
+                      color: 0XFF9F51CA,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Otomanopee One'),
+                ],
+              ),
+              const SizedBox(height: 15.0),
+              const CustomContainerDesplegable(
+                path: 'assets/img/ecografiaObstetrica.jpg',
+                title: 'ECOGRAFÍA OBSTETRICA ',
+                descripcion1: '1. Historia Clínico Electrónica',
+                descripcion2: '2. Evaluación Materna y Fetal',
+                descripcion3: '3. Control Ecográfico Básico',
+              ),
+            ],
+          ),
+        ));
   }
 }
